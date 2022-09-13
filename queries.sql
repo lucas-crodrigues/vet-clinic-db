a65fdb79f08d166386fe53eb67aa73ae0047f1bc
lucascr <lcrodrigues1995@gmail.com>
2022-09-13 17:08:20 -0300

    Query data to answer questions

--- queries.sql
+++ queries.sql
@@ -1,19 +1,12 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-
 SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
-
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
-
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
-
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
-
 SELECT * FROM animals WHERE neutered = true;
-
 SELECT * FROM animals WHERE name != 'Gabumon';
-
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 BEGIN;
@@ -42,3 +35,10 @@ ROLLBACK TO before2022;
 UPDATE animals SET weight_kg = (weight_kg*-1) WHERE weight_kg < 0;
 COMMIT;
 
+SELECT COUNT(*) from animals;
+SELECT COUNT(*) from animals WHERE escape_attempts =0;
+SELECT AVG(weight_kg) from animals;
+SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species,MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species,AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
