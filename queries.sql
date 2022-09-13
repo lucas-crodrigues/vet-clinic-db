8231a5708aa3128bbe1ff8c686c32b9b66226e5f
lucascr <lcrodrigues1995@gmail.com>
2022-09-13 16:56:26 -0300

    Manipulate data

--- queries.sql
+++ queries.sql
@@ -15,3 +15,30 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species='unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SAVEPOINT before2022;
+UPDATE animals SET weight_kg = (weight_kg*-1);
+ROLLBACK TO before2022;
+UPDATE animals SET weight_kg = (weight_kg*-1) WHERE weight_kg < 0;
+COMMIT;
+
