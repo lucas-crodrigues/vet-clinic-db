69276ed336c1cd9cf06478bb51f1bd01dc635466
lucascr <lcrodrigues1995@gmail.com>
2022-09-13 11:48:21 -0300

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+	id serial PRIMARY KEY,
+	name varchar(100) NOT NULL,
+	date_of_birth date NOT NULL,
+	escape_attempts int DEFAULT 0,
+	neutered BOOLEAN NOT NULL,
+	weight_kg NUMERIC(10,2) NOT NULL
 );
