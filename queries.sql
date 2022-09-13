c7da884f5fe194fa0b9a3698d46f818bdfdf8949
lucascr <lcrodrigues1995@gmail.com>
2022-09-13 11:48:40 -0300

    query data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+--SELECT * from animals WHERE name LIKE '%mon';
+
+--SELECT name FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
+
+SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
