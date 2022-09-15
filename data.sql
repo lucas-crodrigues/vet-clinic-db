4be1770080cc1398781f66457f821cbf7d22ca4a
lucascr <lcrodrigues1995@gmail.com>
2022-09-15 18:20:10 -0300

    insert data onto new tables, and update new data onto animals table

--- data.sql
+++ data.sql
@@ -14,3 +14,23 @@ VALUES('Charmander', '08-02-2020', 0, false, -11),
 		('Boarmon', '07-06-2005', 7, true, 20.4),
 		('Blossom', '13-10-1998', 3, true, 17),
 		('Ditto', '14-05-2022', 4, true, 22);
+
+INSERT INTO owners (full_name, age)
+VALUES('Sam Smith', 34 ),
+		('Jennifer Orwell', 19),
+		('Bob ', 45),
+		('Melody Pond', 77),
+		('Dean Winchester', 14),
+		('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+VALUES('Pokemon'),
+		('Digimon');
+
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 1 WHERE species_id IS NULL;
+UPDATE animals SET owners_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owners_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owners_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owners_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owners_id = 5 WHERE name IN ('Angemon', 'Boarmon');
