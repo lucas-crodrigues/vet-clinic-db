ce80a1570a8292b22a1905c4617ce2e14b0915ed
lucascr <lcrodrigues1995@gmail.com>
2022-09-16 16:14:38 -0300

    Create tables: vets; specializations; and visits.

--- schema.sql
+++ schema.sql
@@ -29,3 +29,33 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT f_species_id FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
 
 ALTER TABLE animals ADD COLUMN owners_id INT, ADD CONSTRAINT f_owners_id FOREIGN KEY (owners_id) REFERENCES owners(id) ON DELETE CASCADE;
+
+CREATE TABLE vets (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	age INT NOT NULL,
+	date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+	species_id INT NOT NULL,
+	vets_id INT NOT NULL,
+
+	CONSTRAINT f_species FOREIGN KEY(species_id) REFERENCES species(id) ON DELETE CASCADE,
+
+	CONSTRAINT f_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+
+	PRIMARY KEY(species_id,vets_id)
+);
+
+CREATE TABLE visits (
+  vets_id INT NOT NULL,
+  animals_id INT NOT NULL,
+  date DATE,
+
+  CONSTRAINT f_vets FOREIGN KEY(vets_id) REFERENCES vets(id) ON DELETE CASCADE,
+
+  CONSTRAINT f_animals FOREIGN KEY(animals_id) REFERENCES animals(id) ON DELETE CASCADE,
+
+	PRIMARY KEY(vets_id,animals_id,date)
+);
